127b9b1f3590a1f696fd4ba26222afb506bdc4b1
shubhankar <iamshubhankarkhare@gmail.com>
2024-03-30 02:34:58 +0530

    feat: initial commit with nextjs and shadcn setup

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function SignInPage() {
+  return (
+    <div>
+      SignInPage
+      <Button>Login</Button>
+    </div>
+  )
+}
+
+export default SignInPage
