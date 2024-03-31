ee86fad0d986f3f241b63d5bac992c47bd88672e
shubhankar <iamshubhankarkhare@gmail.com>
2024-03-31 19:14:02 +0530

    feat: real time updates for prices

--- src/components/common/charts/index.tsx
+++ src/components/common/charts/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState, useEffect, useRef } from 'react'
-import { IChartApi, createChart } from 'lightweight-charts'
+import { ISeriesApi, createChart } from 'lightweight-charts'
 import {
   candleStickChartOptions,
   chartBaseOptions,
@@ -14,8 +14,9 @@ const Chart = () => {
   const chartRef = useRef<HTMLDivElement>(null)
   const pricesWs = useRef<WebSocket | null>(null)
   const [selectedCoin, setSelectedCoin] = useState('bitcoin') // Default to Bitcoin
-  const [priceData, setPriceData] = useState<CandleData[] | null>(null)
+  const [priceData, setPriceData] = useState<CandleData[]>([])
   const [error, setError] = useState(null)
+  const candlestickSeriesRef = useRef<ISeriesApi<'Candlestick'> | null>(null)
   const [currentBar, setCurrentBar] = useState<CandleData>({
     open: null,
     high: null,
@@ -26,6 +27,7 @@ const Chart = () => {
 
   // methods
   const fetchPrice = async () => {
+    console.log('fetchPrice')
     try {
       const response = await axios.get(
         `/api/getHistoricalPrices?coin=${selectedCoin}`,
@@ -37,88 +39,87 @@ const Chart = () => {
     }
   }
 
-  const handleCoinChange = () => {
-    if (selectedCoin === 'bitcoin') {
-      setSelectedCoin('ethereum')
-    } else {
-      setSelectedCoin('bitcoin')
-    }
-    fetchPrice()
-  }
-
   const mergeTickToBar = (price: number) => {
-    if (currentBar.open === null) {
-      currentBar.open = price
-      currentBar.high = price
-      currentBar.low = price
-      currentBar.close = price
-    } else {
-      currentBar.close = price
-      currentBar.high = Math.max(currentBar.high, price)
-      currentBar.low = Math.min(currentBar.low, price)
-    }
+    setCurrentBar((prevBar) => {
+      if (prevBar.open === null) {
+        return {
+          open: price,
+          high: price,
+          low: price,
+          close: price,
+          time: priceData[priceData.length - 1]?.time,
+        }
+      } else {
+        const temp = { ...prevBar }
+        temp.close = price
+        temp.high =
+          prevBar.high !== null ? Math.max(prevBar.high, price) : price
+        temp.low = prevBar.low !== null ? Math.min(prevBar.low, price) : price
+
+        if (priceData?.length > 0)
+          temp.time = priceData[priceData.length - 1]?.time
+        return temp
+      }
+    })
   }
 
   // lifecycle
+  useEffect(() => {
+    if (currentBar && candlestickSeriesRef.current) {
+      candlestickSeriesRef.current.update(currentBar)
+    }
+  }, [currentBar])
 
   useEffect(() => {
-    if (!priceData?.length) return
+    console.log('priceData', priceData)
+    if (!chartRef.current || !priceData.length) {
+      return
+    }
+    const chart = createChart(chartRef.current, chartBaseOptions)
+    candlestickSeriesRef.current = chart.addCandlestickSeries(
+      candleStickChartOptions,
+    )
+
+    candlestickSeriesRef.current.setData(priceData)
+    chart.timeScale().fitContent()
 
     if (!pricesWs.current) {
       pricesWs.current = new WebSocket(
-        'wss://ws.coincap.io/prices?assets=bitcoin',
+        `wss://ws.coincap.io/prices?assets=${selectedCoin}`,
       )
 
       pricesWs.current.onmessage = function (msg) {
         // Update the last candlestick with the live price
         if (msg.data) {
-          // convert the price to number which is a string in a float format right now
           const priceObject = JSON.parse(msg.data)
           const currentPrice = parseFloat(priceObject[selectedCoin])
           console.log('currentPrice', currentPrice)
+          mergeTickToBar(currentPrice)
         }
       }
     }
-
     return () => {
       if (pricesWs.current) {
         pricesWs.current.close()
         pricesWs.current = null
       }
+      chart.remove()
     }
   }, [priceData])
 
   useEffect(() => {
-    console.log('priceData', priceData)
-    if (chartRef.current && priceData) {
-      const chart = createChart(chartRef.current, chartBaseOptions)
-      const candlestickSeries = chart.addCandlestickSeries(
-        candleStickChartOptions,
-      )
-      console.log('candlestickSeries', candlestickSeries)
-
-      candlestickSeries.setData(priceData)
-      chart.timeScale().fitContent()
-
-      const handleResize = () => {
-        const width = chartRef?.current?.clientWidth ?? 500
-        chart.applyOptions({ width })
-      }
-
-      window.addEventListener('resize', handleResize)
-
-      return () => {
-        window.removeEventListener('resize', handleResize)
-
-        chart.remove()
-      }
-    }
-  }, [priceData])
+    fetchPrice()
+    console.log('mounted')
+  }, [])
 
   return (
     <div className="py-4 bg-[#1D1F22] w-full h-full min-w-[500px] min-h-[300px] rounded-md flex items-center justify-center">
       <div> {error && <h1 className="text-red-500">{error}</h1>}</div>
-      <Button onClick={handleCoinChange}>Bitcopin</Button>
+      {selectedCoin === 'bitcoin' ? (
+        <h1 className="text-white">Bitcoin</h1>
+      ) : (
+        <h1 className="text-white">Ethereum</h1>
+      )}
       <div className="h-full w-full m-4 " ref={chartRef} />
     </div>
   )
